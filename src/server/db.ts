520620e07c00c811ca831e578d93c94864fe0ad5
m.kolo <kolomoyets473@gmail.com>
2023-12-18 21:39:30 +0200

    test(library)

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-import { env } from '@/env.mjs';
+// import { env } from '@/env.mjs';
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
@@ -13,4 +13,4 @@ export const db =
         // env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
     });
 
-if (env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+globalForPrisma.prisma = db;
