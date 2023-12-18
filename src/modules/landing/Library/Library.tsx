62431c2e00a09dfe0fc6f4f340825cdf52375eb3
m.kolo <kolomoyets473@gmail.com>
2023-12-18 21:42:41 +0200

    test(library)

--- src/modules/landing/Library/Library.tsx
+++ src/modules/landing/Library/Library.tsx
@@ -134,6 +134,7 @@ const LibraryModule: React.FC = () => {
                                                             alt={publication.title}
                                                             width={720 / 1.5}
                                                             height={1280 / 1.5}
+                                                            unoptimized
                                                             placeholder="blur"
                                                             blurDataURL={`data:image/svg+xml;base64,${toBase64(
                                                                 shimmer(720 / 1.5, 1280 / 1.5)
